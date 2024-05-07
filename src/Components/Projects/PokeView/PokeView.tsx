c6481425f24cb8bde3c448f70dfe692de5aa8e75
Hassan Hashmi <hashmihassan47@gmail.com>
2024-05-07 19:04:08 -0400

    pokeview mostly complete

--- src/Components/Projects/PokeView/PokeView.tsx
+++ src/Components/Projects/PokeView/PokeView.tsx
@@ -1,3 +1,4 @@
+import './PokeView.css'
 import pokeViewOne from '../../../assets/pokeViewGeneral.png'
 import pokeViewTwo from '../../../assets/pokeViewCloseUp.png'
 import pokeViewThree from '../../../assets/pokeViewDarkMode.png'
@@ -5,18 +6,23 @@ import pokeViewThree from '../../../assets/pokeViewDarkMode.png'
 
 const PokeView = () => {
   return (
-    <div>        
-      <p>PokeView is a web app to learn more about Pokemon. 
-      It generates pokemon randomly and paints them in a 
-      custom design that mimics the orignal DS games. 
-      There is also a search feature, a dropdown filter, 
-      and a custom dark mode with a shiny surprise.
+    <div className='pokeview'>        
+      <p>
+        PokéView is a web app to learn more about Pokemon. 
+        It generates pokemon randomly and paints them in a 
+        custom design that mimics the orignal Nintendo DS games. 
+      </p> 
+      <img src={pokeViewOne} id='pokeview__image-1'/>
+      <p>
+        Click on a card to open a detailed view with additional stats and a custom graph.
       </p>
-      <div className='projectOne_images'>
-        <img src={pokeViewOne} className='projectOne_images__item-1'/>
-        <img src={pokeViewTwo} className='projectOne_images__item-2'/>
-        <img src={pokeViewThree} className='projectOne_images__item-3'/> 
-      </div></div>
+      <img src={pokeViewTwo} id='pokeview__image-2'/>
+      <p>
+        There's also a search feature, a dropdown filter, 
+        and a custom dark mode with a shiny surprise.
+      </p>
+      <img src={pokeViewThree} id='pokeview__image-3'/> 
+    </div>
   )
 }
 
--- src/Components/Welcome/Welcome.tsx
+++ src/Components/Welcome/Welcome.tsx
@@ -5,16 +5,16 @@ const Welcome = () => {
     <div className='welcome-container'>
       
       <p>
-        Back in 2015, during a particularly late night I messed around with Roblox Studio to try and make a game. 
-        I was mesmerized by the notion of bringing my wildest, most intrusive ideas to life. 
+        Back in 2015, I messed around with Roblox Studio to try and make a game to make me Robux Rich. 
+        Beyond greed, I was motivated by the notion of bringing my wildest, most intrusive ideas to life. 
       </p>
       <p>
-        At some point during my confused blundering of the complex Game-dev tool, I realized that I was more interested in the code than the game itself.
+        At some point during my confused blundering, I realized that I was more interested in the code than the game itself.
         I found true joy in the intersection of video game aesthetics and the accessibility of the web.
-        Fast-forward to today, and I've built software for a GIS company, my local badminton club, and for all my fellow Pokemon fans.
+        Fast-forward to today, and I've built web software for a GIS company, my local badminton club, and for all my fellow Pokémon fans.
       </p>
       <p>
-        I still play video games, and am currently working on a game of my own. Otherwise you can find me straining in the gym, 
+        To this day I still play video games, and I hope to one day release my own games. Otherwise you can find me straining in the gym, 
         playing badminton, bouldering, or goofing off with my friends and family.
       </p>
       
