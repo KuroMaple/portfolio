6c04edac90f3429256286617be55836ff1ef9f24
Hassan Hashmi <hashmihassan47@gmail.com>
2024-06-24 20:56:47 -0400

    feat: nav bar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,15 +1,34 @@
+import { useState } from 'react'
 import './NavBar.css'
 
 const NavBar = () => {
+  const [active, setActive] = useState(0)
+  const [hovered, setHovered] = useState(-1)
+
+  const renderlistElement = (index: number, text: string) => {
+    return (
+      <li 
+        onClick={() => setActive(index)}
+        onMouseEnter={() => setHovered(index)}
+        onMouseLeave={() => setHovered(-1)}
+      >
+        <a className='nav-bar__item' >
+          {(active === index || hovered === index) ? 
+            (<span className='nav-bar__indicator-long'>&nbsp;</span>) : 
+            (<span className='nav-bar__indicator-short'>&nbsp;</span>)
+          }
+          <span>{text}</span>
+        </a>
+      </li>
+    )
+  }
+
   return (
     <nav className='header__nav-bar'>
       <ul className='nav-bar__list'>
-        <li className='nav-bar__item'>
-          <span className='nav-bar__indicator'>&nbsp;</span>
-          <span>ABOUT</span>
-        </li>
-        <li className='nav-bar__item'>EXPERIENCE</li>
-        <li className='nav-bar__item'>PROJECTS</li>
+        {renderlistElement(0, 'ABOUT')}
+        {renderlistElement(1, 'EXPERIENCE')}
+        {renderlistElement(2, 'PROJECTS')}
       </ul>
     </nav>
   )
