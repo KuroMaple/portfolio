122640dd9d5a82e84a945f5b6d1bdf62e06f5268
Hassan Hashmi <hashmihassan47@gmail.com>
2025-01-25 16:35:18 -0500

    add profile picture and github links

--- src/Components/Projects/ProjectsList.tsx
+++ src/Components/Projects/ProjectsList.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import pokeball from '../../assets/pokeballIcon.png'
 import badminton from '../../assets/badmintonIcon.png'
+import linkIcon from '../../assets/linkIcon.png'
 
 import "./ProjectList.css"
 import { ContentType } from '../../App'
@@ -13,13 +14,23 @@ const ProjectsList: React.FC<Props> = ({setCurrentContent}) => {
     <div className='container'>
       
       <ul>
-        <li className='list-item' onClick={() => setCurrentContent('Project1')}>
-          <img src={pokeball} className='list-item-icon'/>
-          PokéView
+        <li className='list-item'>
+          <div className='project-text' onClick={() => setCurrentContent('Project1')}>
+            <img src={pokeball} className='list-item-icon'/>
+            PokéView
+          </div>
+          <a href="https://github.com/KuroMaple/Poke-View" target="_blank" rel="noopener noreferrer" className="github-btn">
+            <img className="github-icon" src={linkIcon} alt="GitHub" />
+          </a>
         </li>
-        <li className='list-item' onClick={() => setCurrentContent('Project2')}>
-          <img src={badminton} className='list-item-icon'/>
-          UWBC Web
+        <li className='list-item'>
+          <div className="project-text" onClick={() => setCurrentContent('Project2')}>
+            <img src={badminton} className="list-item-icon" alt="UWBC Web Icon" />
+        UWBC Web
+          </div>
+          <a href="https://github.com/KuroMaple/uwbc-web" target="_blank" rel="noopener noreferrer" className="github-btn">
+            <img className="github-icon" src={linkIcon} alt="GitHub" />
+          </a>
         </li>
       </ul>
     </div>
--- src/Components/Welcome/Welcome.tsx
+++ src/Components/Welcome/Welcome.tsx
@@ -1,33 +1,48 @@
 import './Welcome.css'
-
+import facePic from '../../assets/facePic.jpg'
 const Welcome = () => {
   return (
     <div className='welcome-container'>
-      
+      <img 
+        src={facePic} 
+        alt="Profile Picture" 
+        style={{ 
+          width: '100%', // Adjust to the container's full width 
+          maxWidth: '300px', // Prevent it from being too large
+          height: 'auto', // Maintain aspect ratio
+          borderRadius: '10%', // Slightly rounded corners
+          objectFit: 'cover', // Crop image to fit perfectly
+          display: 'block', // Prevent inline spacing
+          margin: '0 auto', // Center the image horizontally
+        }} 
+      />
       <p>
-        Back in 2015, I messed around with&nbsp;
+        Back in 2015, I discovered&nbsp;
         <a href='https://create.roblox.com' className='welcome__text-links' id='welcome-roblox' target='_blank'>
           Roblox Studio
         </a>
-        &nbsp;to try and make a game to make me Robux Rich. 
-        Beyond greed, I was motivated by the notion of bringing my wildest, most intrusive ideas to life. 
+        &nbsp;and set out to create a game that would make me "Robux Rich." 
+        While initially driven by  greed, I quickly became captivated by the process of coding and the ability to bring my most creative and unconventional ideas to life. 
       </p>
       <p>
-        At some point during my confused blundering, I realized that I was more interested in the code than the game itself.
-        I found true joy in the intersection of video game aesthetics and the accessibility of the web.
-        Fast-forward to today, and I've built web software for a&nbsp;
+        This early exploration sparked my passion for software development, especially at the intersection of engaging design and accessible web technologies.
+        Since then, I've had the privilege of building web software for a&nbsp;
         <a href='https://www.janacorporation.com' className='welcome__text-links' target='_blank'>GIS company</a>,&nbsp;
-        my&nbsp;
+        my university's&nbsp;
         <a href='https://uwbc-web-demo.netlify.app' className='welcome__text-links' target='_blank' >badminton</a> 
-        &nbsp;club, and for all my fellow&nbsp;
+        &nbsp;club, and a project dedicated to&nbsp;
         <a href='https://poke-view.netlify.app' className='welcome__text-links' target='_blank' >Pokémon</a> 
         &nbsp;fans.
       </p>
       <p>
-        To this day I still play video games, and I hope to one day release my own games. Otherwise you can find me straining in the gym, 
-        playing badminton, bouldering, or goofing off with my friends and family.
+      Today, I'm passionate about developing interactive 
+      and user-friendly applications, whether that's through 
+      front-end development, full-stack solutions, or even game design. 
+      When I'm not coding, you'll find me pushing my limits at the gym, 
+      competing on the badminton court, bouldering, or spending quality time with friends and family.
       </p>
       
+
     </div>
   )
 }
