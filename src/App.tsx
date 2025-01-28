7454b01a68dca389e4009e37b1980e14cd143c03
Hassan Hashmi <hashmihassan47@gmail.com>
2025-01-27 22:42:19 -0500

    done scroll bar

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ function App() {
         <NavBar />
         <About />
         <Technologies />
-        {/* <Projects />
-        <Contact /> */}
+        <Projects />
+        {/* <Contact /> */}
       </main>
     </>
   )
