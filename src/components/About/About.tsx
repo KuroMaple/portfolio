428f36d7ae562b839d3ca4eec3815a0a6f24a957
Hassan Hashmi <hashmihassan47@gmail.com>
2025-01-27 20:50:29 -0500

    done about section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,4 +1,6 @@
 import headshot from '../../assets/facePic.png'
+import { motion } from 'motion/react'
+
 const About = () => {
   return (
     <div 
@@ -8,16 +10,25 @@ const About = () => {
       <div
         className='flex flex-col items-center justify-center gap-10 text-white'
       >
-        <div>
+        <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+        >
           <img 
             src={headshot} 
             className='w-[300px] cursor-pointer rounded-full shadow-xl 
           shadow-emerald-800 transition-all duration-300 hover:-translate-y-5 
           hover:scale-105 hover:shadow-2xl hover:shadow-cyan-600 md:w-[350px] select-none'  
           />
-        </div>
+        </motion.div>
         
-        <div className='flex max-w-[600px] flex-col items-center justify-center gap-3 text-center'>
+        <motion.div 
+          className='flex max-w-[600px] flex-col items-center justify-center gap-3 text-center'
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8, delay: 0.4 }} 
+        >
           <h1
             className='bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent opacity-80 text-5xl select-none font-light md:text-7xl'
           >
@@ -42,7 +53,7 @@ const About = () => {
               className="hover:bg-gradient-to-r hover:from-green-100 hover:to-green-500 hover:text-transparent hover:bg-clip-text" 
               target="_blank">GIS company</a>,&nbsp;
             my&nbsp;
-            <a href="https://uwbc-web-demo.netlify.app" 
+            <a href="https://badmintonclub.uwaterloo.ca" 
               className="hover:bg-gradient-to-r hover:from-cyan-100 hover:to-cyan-500 hover:text-transparent hover:bg-clip-text" 
               target="_blank">university badminton club</a> 
             &nbsp;, and my fellow&nbsp;
@@ -51,7 +62,7 @@ const About = () => {
               target="_blank">Pokémon fans</a>
             &nbsp;.
           </p>
-        </div>
+        </motion.div>
       </div>
 
     </div>
