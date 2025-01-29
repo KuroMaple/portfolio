da299b92c3e27f349516bf973a2440c46e8e2c0f
Hassan Hashmi <hashmihassan47@gmail.com>
2025-01-28 22:06:35 -0500

    chore(About): update wording and add html anchor

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,3 +1,4 @@
+import { hr } from 'motion/react-client'
 import headshot from '../../assets/facePic.png'
 import { motion } from 'motion/react'
 
@@ -7,22 +8,24 @@ const About = () => {
       id='about'
       className='px-16 flex min-h-screen w-full items-center justify-center py-28 md:px-32'  
     >
+      
       <div
         className='flex flex-col items-center justify-center gap-10 text-white'
       >
-        <motion.div
-          initial={{ opacity: 0, x: 100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.8, delay: 0.2 }}
-        >
-          <img 
-            src={headshot} 
-            className='w-[300px] cursor-pointer rounded-full shadow-xl 
+        <a href='#contact'>
+          <motion.div
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+          >
+            <img 
+              src={headshot} 
+              className='w-[300px] cursor-pointer rounded-full shadow-xl 
           shadow-emerald-800 transition-all duration-300 hover:-translate-y-5 
           hover:scale-105 hover:shadow-2xl hover:shadow-cyan-600 md:w-[350px] select-none'  
-          />
-        </motion.div>
-        
+            />
+          </motion.div>
+        </a>
         <motion.div 
           className='flex max-w-[600px] flex-col items-center justify-center gap-3 text-center'
           initial={{ opacity: 0, x: -100 }}
@@ -51,7 +54,7 @@ const About = () => {
             Passionate developer whose had the privilege of building software for a&nbsp;
             <a href="https://www.janacorporation.com" 
               className="bg-gradient-to-r hover:from-green-100 hover:to-green-500 text-transparent bg-clip-text from-green-500 to-green-100" 
-              target="_blank">GIS company</a>,&nbsp;
+              target="_blank">SaaS company</a>,&nbsp;
             my&nbsp;
             <a href="https://badmintonclub.uwaterloo.ca" 
               className="bg-gradient-to-r hover:from-cyan-100 hover:to-cyan-500 text-transparent bg-clip-text from-cyan-500 to-cyan-100" 
