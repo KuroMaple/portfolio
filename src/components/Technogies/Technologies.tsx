5e4ec4827e656e70fcce41d600529b7291a4b80e
Hassan Hashmi <hashmihassan47@gmail.com>
2025-10-26 15:07:51 -0400

    Resolve build errors

--- src/components/Technogies/Technologies.tsx
+++ src/components/Technogies/Technologies.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'motion/react'
-import { BiLogoMongodb, BiLogoTypescript } from 'react-icons/bi'
+import { BiLogoTypescript } from 'react-icons/bi'
 import { FaReact } from 'react-icons/fa'
 import { RiTailwindCssFill } from 'react-icons/ri'
 import { SiFlutter } from 'react-icons/si'
